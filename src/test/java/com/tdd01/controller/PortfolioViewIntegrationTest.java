9e423f75637256c9ae9d053cc3e9580f76e06839
narraboss <basanth.narra@gmail.com>
2020-10-03 16:57:22 -0400

    first test case

---
+++ src/test/java/com/tdd01/controller/PortfolioViewIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.tdd01.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest
+public class PortfolioViewIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void givenPortfolioWithOpenPositionDisplaysPosition() throws Exception {
+        //get /view
+        MvcResult results = mockMvc.perform(MockMvcRequestBuilders.get("/view"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("view"))
+                .andExpect(model().attributeExists("positions"))
+                .andReturn();
+
+        //check for model containing attributes
+        List<String> positions = (List<String>) results.getModelAndView()
+                .getModel()
+                .get("positions");
+        Assertions.assertThat(positions).hasSize(1);
+
+    }
+}
