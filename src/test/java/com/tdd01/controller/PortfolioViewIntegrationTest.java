4b32a23d541d90e92dc23382e92460c86799112d
narraboss <basanth.narra@gmail.com>
2020-10-03 16:59:40 -0400

    first test case

--- src/test/java/com/tdd01/controller/PortfolioViewIntegrationTest.java
+++ src/test/java/com/tdd01/controller/PortfolioViewIntegrationTest.java
@@ -4,13 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.List;
 
